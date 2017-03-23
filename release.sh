3f2f3c7ce1c62f224231a99eac0ea74f2ffbb4d6
Nathan Bleigh <nathan.bleigh@gmail.com>
2017-03-23 15:24:15 -0500

    Initial Commit

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-tar -zcvf dist/aws-rollout-linux-x86_64.tar.gz dist/aws-rollout
+tar -zcvf dist/aws-rollout-darwin-x86_64.tar.gz dist/aws-rollout
 latest_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
 github-release "oberd/ecsy" "$latest_tag" "$(git rev-parse --abbrev-ref HEAD)" "" 'dist/*.tar.gz'
