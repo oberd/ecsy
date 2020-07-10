095b8911679344c3a5baf166d2ea2a0c6a90e5a9
Nathan Bleigh <nathan.bleigh@gmail.com>
2020-07-09 20:05:21 -0500

    update to gomodules, adds linux support to release

--- release.sh
+++ release.sh
@@ -7,4 +7,4 @@ DOCKER_IMAGE_NAME="oberd/ecsy:${LAST_TAG:1}"
 docker build -t "$DOCKER_IMAGE_NAME" .
 docker push "$DOCKER_IMAGE_NAME"
 
-github-release "oberd/ecsy" "$LAST_TAG" "$(git rev-parse --abbrev-ref HEAD)" "" "dist/ecsy-$LAST_TAG-darwin-amd64"
+github-release "oberd/ecsy" "$LAST_TAG" "$(git rev-parse --abbrev-ref HEAD)" "" "dist/ecsy-$LAST_TAG-*"
