b82c251e502589c15af4223be52a74cd02a6c105
Nathan Bleigh <nathan.bleigh@gmail.com>
2020-09-04 13:49:21 -0500

    release var

--- release.sh
+++ release.sh
@@ -4,6 +4,7 @@ set -e
 
 LAST_TAG=$(git describe --abbrev=0 --tags)
 DOCKER_IMAGE_NAME="oberd/ecsy:${LAST_TAG:1}"
+export GITHUB_TOKEN="${GITHUB_TOKEN:-$GITHUB_OAUTH_TOKEN}"
 docker build -t "$DOCKER_IMAGE_NAME" .
 docker push "$DOCKER_IMAGE_NAME"
 
