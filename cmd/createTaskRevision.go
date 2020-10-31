dd4821a78f180f9d200bc4a478f9e642788cfcc6
Nathan Bleigh <nathan.bleigh@gmail.com>
2020-10-31 09:10:00 -0500

    adds locate task option to create-task-revision

--- cmd/createTaskRevision.go
+++ cmd/createTaskRevision.go
@@ -1,33 +1,35 @@
 package cmd
 
 import (
-    "fmt"
-    "github.com/oberd/ecsy/ecs"
-    "github.com/spf13/cobra"
+	"fmt"
+
+	"github.com/oberd/ecsy/ecs"
+	"github.com/spf13/cobra"
 )
 
 var imageUrl string
 
 // createTaskRevisionCmd represents the createTaskRevision command
 var createTaskRevisionCmd = &cobra.Command{
-	Use:   "create-task-revision [cluster] [service] --image",
+	Use:   "create-task-revision [cluster] [service] --image --task-definition-source",
 	Short: "duplicate a task definition into a new revision with a different image",
-	Long: ``,
+	Long:  ``,
 	RunE: func(cmd *cobra.Command, args []string) error {
-        def, err := ecs.GetCurrentTaskDefinition(args[0], args[1])
-        if err != nil {
-            return err
-        }
-        newTask, err := ecs.CreateNewTaskWithImage(def, imageUrl)
-        if err != nil {
-            return err
-        }
-        fmt.Printf("%s", *newTask.TaskDefinitionArn)
-        return nil
+		def, err := ecs.LocateTaskDef(args[0], args[1], taskDefinitionSource)
+		if err != nil {
+			return err
+		}
+		newTask, err := ecs.CreateNewTaskWithImage(def, imageUrl)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("%s", *newTask.TaskDefinitionArn)
+		return nil
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(createTaskRevisionCmd)
+	createTaskRevisionCmd.Flags().StringVarP(&taskDefinitionSource, "task-definition-source", "s", "newest", `locator for task definition ("newest" will use the newest in the family of the service, "current" will use the currently deployed)`)
 	createTaskRevisionCmd.Flags().StringVarP(&imageUrl, "image", "i", "", "Image to update into the active container definition")
 }
